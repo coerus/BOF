c3b933f542391cadd17ef76fe8021c4103218edf
enso <77758986+ennso@users.noreply.github.com>
2022-04-14 10:03:51 +0000

    Add files via upload

---
+++ BOF.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+#include<stdio.h>
+
+#include<string.h>
+
+void xplBOF(char *input);
+
+int main(int argc, char *argv[])
+
+{
+
+if(argc>1)
+
+xplBOF(argv[1]);
+
+}
+
+void xplBOF(char*input)
+
+{
+
+char buffer[256];
+
+strcpy(buffer, input);
+
+}
